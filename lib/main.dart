59ffde5857fcf04f82e0d8ac255ce32b353a56bb
Carlos Buruel <charly.ob.26@gmail.com>
2025-03-06 21:08:31 -0600

    [CBo] Show Main screen by flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:starving_shopping_flutter_app/config/constant/environment.dart';
+import 'package:starving_shopping_flutter_app/presentation/screens/prod_screens.dart';
 import 'package:starving_shopping_flutter_app/shared/examples/screens/examples_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await Environment.initEnvironment();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ExamplesScreen(),
+      home:
+          Environment.wantSeeExamples
+              ? const MainScreen()
+              : const ExamplesScreen(),
     );
   }
 }

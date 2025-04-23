d41d00aa99ec64eca38399ea99643b33c32722f3
carlosburuelortiz <94727269+carlosburuelortiz@users.noreply.github.com>
2025-04-22 19:19:26 -0600

    [BOc] integrate bloc, injector, freezed (#9)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:starving_shopping_flutter_app/code/di/injector.dart';
 
 import 'package:starving_shopping_flutter_app/config/constant/environment.dart';
+import 'package:starving_shopping_flutter_app/presentation/blocs/grocery_bloc.dart';
+import 'package:starving_shopping_flutter_app/presentation/blocs/grocery_event.dart';
 import 'package:starving_shopping_flutter_app/presentation/screens/main_screen.dart';
 import 'package:starving_shopping_flutter_app/shared/examples/screens/examples_screen.dart';
 
 void main() async {
   await Environment.initEnvironment();
+  setupLocator();
 
-  runApp(
-    const ProviderScope(
-      child: MyApp()
-    )
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,12 +20,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:
-          Environment.wantSeeExamples
-              ? const ExamplesScreen()
-              : const MainScreen(),
+    return BlocProvider(
+      create:
+          (context) =>
+              getIt<GroceryBloc>()..add(const GroceryEvent.loadGroceries()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:
+            Environment.wantSeeExamples
+                ? const ExamplesScreen()
+                : const MainScreen(),
+      ),
     );
   }
 }
---
+++ lib/presentation/blocs/grocery_bloc.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:starving_shopping_flutter_app/domain/entity/grocery.dart';
+import 'package:starving_shopping_flutter_app/domain/repository/local_storage_repository.dart';
+
+import 'grocery_event.dart';
+import 'grocery_state.dart';
+
+class GroceryBloc extends Bloc<GroceryEvent, GroceryState> {
+  final LocalStorageRepository localStorageRepository;
+
+  GroceryBloc({required this.localStorageRepository}) : super(GroceryLoading()) {
+    on<LoadGroceries>(onLoadGroceries);
+    on<AddGrocery>(onAddGrocery);
+  }
+
+  FutureOr<void> onLoadGroceries(LoadGroceries event, Emitter<GroceryState> emit) async {
+    emit(GroceryLoading());
+    
+    try {
+      Future.delayed(const Duration(seconds: 2));
+      final groceryList = await localStorageRepository.getAllGroceries();
+
+      if (groceryList.isEmpty) {
+        emit(GroceryLoadedEmpty());
+        return;
+      } else {
+        emit(GroceryLoadedNotEmpty(groceryList));
+      }
+    } catch (e) {
+      emit(GroceryError(e.toString()));
+    }
+  }
+
+  FutureOr<void> onAddGrocery(AddGrocery event, Emitter<GroceryState> emit) async {
+    emit(GroceryLoading());
+    
+    try {
+      final grocery = Grocery(name: event.groceryName);
+      await localStorageRepository.putGrocery(grocery);
+      final groceryList = await localStorageRepository.getAllGroceries();
+      emit(GroceryLoadedNotEmpty(groceryList));
+    } catch (e) {
+      emit(GroceryError(e.toString()));
+    }
+  }
+}
